351340422b99b284b7cc7b7b9dfbfe5796e37e42
Gorakhnath Ghadage <innovating.solution@gmail.com>
2021-02-14 14:59:55 +0530

    Implemented Course Component

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
 
 @Component({
-  selector: 'app-course',
+  // tslint:disable-next-line: component-selector
+  selector: 'course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
 
-  constructor() { }
+  course!: Course;
+
+  displayedColumns = ['seqNo', 'description', 'duration'];
+
+  dataSource: any;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+
+  loadMore(): void {
+
   }
 
 }
