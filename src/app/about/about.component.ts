b09809bda6e797428d8ab915ad9035e624c84972
Gorakhnath Ghadage <innovating.solution@gmail.com>
2021-02-15 07:39:10 +0530

    Angular Fire & ValueChanges Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,23 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 import { Course } from '../model/course';
-
-const config = {
-  apiKey: 'AIzaSyBM9QZTchI2vXuW8dvtiVMbIa6JJWrsU6A',
-  authDomain: 'fir-course-510bc.firebaseapp.com',
-  projectId: 'fir-course-510bc',
-  storageBucket: 'fir-course-510bc.appspot.com',
-  messagingSenderId: '207791867774',
-  appId: '1:207791867774:web:5a73da7e37d222274a152f'
-};
-
-firebase.default.initializeApp(config);
-
-const db = firebase.default.firestore();
-
-
 
 @Component({
 
@@ -27,28 +13,11 @@ const db = firebase.default.firestore();
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit(): void {
-
-    db.doc('courses/sHwpPTsAlpBEOpTz8hHS').get().then(
-      snap => {
-        console.log(snap.data());
-      }
-    );
-
-    db.collection('courses').get()
-      .then(snaps => {
-        console.log(snaps.docs.map(snap => snap.data()));
-        console.log(snaps.docs.map(snap => snap.id));
-        const courses: Course[] = snaps.docs.map(snap => {
-          return {
-            id: snap.id,
-            ...snap.data()
-          } as Course;
-        });
-        console.log(courses);
-      });
+    this.db.collection('courses').valueChanges()
+    .subscribe(console.log);
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-
+    apiKey: 'AIzaSyBM9QZTchI2vXuW8dvtiVMbIa6JJWrsU6A',
+    authDomain: 'fir-course-510bc.firebaseapp.com',
+    projectId: 'fir-course-510bc',
+    storageBucket: 'fir-course-510bc.appspot.com',
+    messagingSenderId: '207791867774',
+    appId: '1:207791867774:web:5a73da7e37d222274a152f'
   }
 };
 
