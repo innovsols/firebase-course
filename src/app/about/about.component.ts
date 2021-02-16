371a3d89797557f9161ebda302cc3aeffb46a9e6
Gorakhnath Ghadage <innovating.solution@gmail.com>
2021-02-16 10:58:35 +0530

    Batch Writes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,6 +3,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import { of } from 'rxjs';
 import { Course } from '../model/course';
 
 @Component({
@@ -17,7 +18,26 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
 
-    // statechanges uses modified state of documents and provides the same.
+
   }
 
+  save(): void {
+    const firebaseCourseRef =
+      this.db.doc('/courses/sHwpPTsAlpBEOpTz8hHS').ref;
+
+    const rxjsCourseRef =
+      this.db.doc('/courses/DpI50HhmagU0IokQRGVu').ref;
+
+    const batch = this.db.firestore.batch();
+
+    batch.update(firebaseCourseRef, { titles: { description: 'Firebase Course',
+      longDescription: 'Serveless Angular with Firestore, Firebase Storage & Hosting, Firebase Cloud Functions & AngularFire' } });
+
+    batch.update(rxjsCourseRef, { titles: { description: 'RxJs Course' ,
+      longDescription: 'Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples'} });
+
+    const batch$ = of(batch.commit());
+
+    batch$.subscribe();
+  }
 }
