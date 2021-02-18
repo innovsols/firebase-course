a0c6d5fd5d555ce9779983e1dd666eabcd79e9c9
Gorakhnath Ghadage <innovating.solution@gmail.com>
2021-02-18 10:05:46 +0530

    User Profile implementations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'firebase-course';
 
-  logout(): void {
+  loggedin$: Observable<boolean>;
 
+  loggedOut$: Observable<boolean>;
+
+  pictureUrl$: Observable<string>;
+
+  constructor(private afAuth: AngularFireAuth) {
+
+  }
+  ngOnInit(): void {
+    this.afAuth.authState.subscribe(console.log);
+
+    this.loggedin$ = this.afAuth.authState.pipe(map(user => !!user));
+
+    this.loggedOut$ = this.loggedin$.pipe(map(loggedin => !loggedin));
+
+    this.pictureUrl$ = this.afAuth.authState.pipe(map(user => user?.photoURL));
+
+  }
+
+  logout(): void {
+  this.afAuth.signOut();
   }
 }
